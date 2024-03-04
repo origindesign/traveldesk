109b5d6e1d1dee969d20c99eebb01c800e1d2638
Sam Medhurst <sammedhurst777@gmail.com>
2024-03-04 14:35:08 -0800

    Build gallery section.

---
+++ web/app/themes/origin/patterns/footer-contact.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Title: Footer Contact
+ * Slug: origin/footer-contact
+ * Categories: theme, travel-desk
+ * Block Types: core/group
+ *
+ * @package origin
+ * @since 1.0.0
+ */
+
+?>
+
+<!-- wp:group {"templateLock":"contentOnly", "className":"is-style-gallery is-full-width-container fade-in","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-gallery is-full-width-container">
+	<!-- wp:columns {"isStackedOnMobile":false} -->
+	<div class="wp-block-columns is-not-stacked-on-mobile">
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:image {"id":107,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="https://trd.lndo.site//app/uploads/2024/03/gallery-1.jpg" alt="" class="wp-image-107" style="aspect-ratio:1;object-fit:cover"/></figure>
+			<!-- /wp:image -->
+			<!-- wp:image {"id":108,"width":"318px","sizeSlug":"medium","linkDestination":"none"} -->
+			<figure class="wp-block-image size-medium is-resized"><img src="https://trd.lndo.site//app/uploads/2024/03/gallery-2-300x186.png" alt="" class="wp-image-108" style="width:318px"/></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:heading -->
+			<h2 class="wp-block-heading">Inside <sub>TravelDesk</sub></h2>
+			<!-- /wp:heading -->
+			<!-- wp:image {"id":109,"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="https://trd.lndo.site//app/uploads/2024/03/gallery-3.png" alt="" class="wp-image-109"/></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+	<!-- wp:group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph -->
+		<p>Wpharetra mattis leo fermentum egestas. Integer consectetur sit diam in in porttitor nisl et. Netus libero morbi proin amet tortor.</p>
+		<!-- /wp:paragraph -->
+		<!-- wp:buttons -->
+		<div class="wp-block-buttons">
+			<!-- wp:button -->
+			<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Explore</a></div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
