75fe35a82e781d495483b0571d69608b47f42a5f
Sam Medhurst <sammedhurst777@gmail.com>
2024-03-06 11:08:33 -0800

    Build client list section and style.

---
+++ web/app/themes/origin/patterns/client-list.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Title: Client List
+ * Slug: origin/faq
+ * Categories: theme, travel-desk
+ * Block Types: core/group
+ *
+ * @package origin
+ * @since 1.0.0
+ */
+
+?>
+
+<!-- wp:group {"templateLock":"contentOnly",  "className":"is-full-width-container is-style-client-list-wrapper fade-in","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-full-width-container is-style-client-list-wrapper fade-in">
+	<!-- wp:group {"className":"is-style-client-list","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group is-style-client-list">
+		<!-- wp:heading {"className":"is-style-keyline-heading"} -->
+		<h2 class="wp-block-heading is-style-keyline-heading">Our Clients</h2>
+		<!-- /wp:heading -->
+		<!-- wp:group {"className":"is-style-client-list-internal","layout":{"type":"constrained"}} -->
+		<div class="wp-block-group is-style-client-list-internal">
+			<!-- wp:paragraph -->
+			<p>european travel commission</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>Choice hotels</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>DESTINATION DC</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>VISIT BRITAIN</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>Choice hotels</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>DESTINATION DC</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>VISIT BRITAIN</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>Choice hotels</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>DESTINATION DC</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>VISIT BRITAIN</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
