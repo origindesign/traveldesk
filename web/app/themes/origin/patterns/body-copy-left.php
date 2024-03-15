58f50d8ca6c4e3789de1afa6ce3d37c64e92caae
Sam Medhurst <sammedhurst777@gmail.com>
2024-03-15 12:12:34 -0700

    build body copy variation structure.

---
+++ web/app/themes/origin/patterns/body-copy-left.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Body Copy Left
+ * Slug: origin/body-copy-left
+ * Categories: theme, travel-desk
+ * Block Types: core/group
+ *
+ * @package origin
+ * @since 1.0.0
+ */
+
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","className":"is-style-body-text-left","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-body-text-left">
+	<!-- wp:heading {"className":"is-style-default"} -->
+	<h2 class="wp-block-heading is-style-default">Body Copy Left Subheading</h2>
+	<!-- /wp:heading -->
+	<!-- wp:heading {"level":3} -->
+	<h3 class="wp-block-heading">Body Copy Left Heading</h3>
+	<!-- /wp:heading -->
+	<!-- wp:paragraph -->
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
